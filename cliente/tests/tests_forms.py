51b26f44a7dfb8f9198c363c66b630786aeb8762
Leonardo Fernandes <leofernandes2169@gmail.com>
2021-12-13 08:40:46 -0300

    Testes

--- cliente/tests/tests_forms.py
+++ cliente/tests/tests_forms.py
@@ -5,13 +5,6 @@ from ..forms import ClienteForm
 
 class ClienteFormTestCase(TestCase):
     
-   # def test_cliente_form_valido(self):
-     #   form = ClienteForm(data={
-      #      'nome_completo' : "Leonardo Fernandes",
-       #     'data_nascimento' : "01-01-1998",
-        #    'ativo' : "Sim"
-    #})
-    #self.assertTrue(forms.is_valid())
 
     def test_cliente_form_valido(self):
         form = ClienteForm(data={})
